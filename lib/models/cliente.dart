3c6c69072bc13a99ce1a748ac44d371cca47d36a
sabrina702 <sabrinassgs101@gmail.com>
2025-09-15 09:53:21 -0300

    feat cliente

--- lib/models/cliente.dart
+++ lib/models/cliente.dart
@@ -5,6 +5,15 @@ class Cliente {
   Carrinho carrinho;
 
   Cliente({required this.nome}) : carrinho = Carrinho();
+
+  String get getnome => nome;
+
+  set setnome(String novoNome) {
+    if (novoNome.trim().isEmpty) {
+      throw Exception('Nome inválido.');
+    }
+    nome = novoNome;
+  }
   
 
 }
